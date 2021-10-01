ae9d215819cbc342403c7c8a546ef3452d0dcf99
MarcosFonseca <marcosfonceka@gmail.com>
2021-09-30 21:13:25 -0300

    Adicionar arquivos de projeto.

---
+++ src/Mercadinho.Prateleira.Domain/Estoque.cs
@@ -0,0 +1,13 @@
+﻿namespace Mercadinho.Prateleira.Domain
+{
+    public class Estoque
+    {
+        public int Id { get; set; }
+
+        public Produto Produto { get; set; }
+
+        public int ProdutoId { get; set; }
+
+        public Qualitativo InfoCompra { get; set; }
+    }
+}
