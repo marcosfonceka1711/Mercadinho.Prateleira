7d87bea4c8f50ea5a1b60ede7049956422e2557e
MarcosFonseca <marcosfonceka@gmail.com>
2021-09-30 21:18:26 -0300

    Remoção de Using não utilizados das classes Domain.

--- src/Mercadinho.Prateleira.Domain/Qualitativo.cs
+++ src/Mercadinho.Prateleira.Domain/Qualitativo.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mercadinho.Prateleira.Domain
+﻿namespace Mercadinho.Prateleira.Domain
 {
     public class Qualitativo
     {
