36df7cbf97f22fab842953b30493afc6184db706
MarcosFonseca <marcosfonceka@gmail.com>
2021-10-05 20:00:30 -0300

    Criação da String de conexão no appsettings.json Configuração ConfigureServices (startup.cs) com a string de conexão Criação da Migration inicial.

--- src/Mercadinho.Prateleira.API/Startup.cs
+++ src/Mercadinho.Prateleira.API/Startup.cs
@@ -1,6 +1,8 @@
+using Mercadinho.Prateleira.Infrastructure.Data.DataRegistration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +14,17 @@ namespace Mercadinho.Prateleira.API
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration collection)
+        {
+            this._configuration = collection;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDataRegistration(_configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- src/Mercadinho.Prateleira.Infrastructure.Data/DataMappings/EstoqueMapping.cs
+++ src/Mercadinho.Prateleira.Infrastructure.Data/DataMappings/EstoqueMapping.cs
@@ -13,11 +13,11 @@ namespace Mercadinho.Prateleira.Infrastructure.Data.DataMappings
     {
         public void Configure(EntityTypeBuilder<Estoque> builder)
         {
-            builder.ToTable("ESTOQUE")
-                .OwnsOne(n => n.InfoCompra)
-                .Property(p => p.PrecoUnidade)
+            builder.ToTable("ESTOQUE")//Nome da tabela
+                .OwnsOne(n => n.InfoCompra)//Faz com que as propriedades da classe Qualitativo (infoCompra) sejam criadas em formato de coluna na tabela estoque
+                .Property(p => p.PrecoUnidade)//Propridade com nome específico
                 .HasColumnName("PRECO_UNITARIO")
-                .HasPrecision(16, 4);
+                .HasPrecision(16, 4);//Com precisão de 4 casas decimais
 
             builder.OwnsOne(n => n.InfoCompra)
                 .Property(p => p.Quantidade)
@@ -25,7 +25,7 @@ namespace Mercadinho.Prateleira.Infrastructure.Data.DataMappings
 
             builder.OwnsOne(n => n.InfoCompra)
                 .Property(p => p.UnidadeMedida)
-                .HasColumnName("UNIDADE_MEDIDA");
+                .HasColumnName("UNIDADE_MEDIDA");//Vai criar como um campo inteiro por ser um Enumaration
 
             builder.HasOne(n => n.Produto)
                 .WithOne(n => n.Estoque)
