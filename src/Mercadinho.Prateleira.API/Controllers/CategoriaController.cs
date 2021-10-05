46d855e6a21a557043f1dfd96c61fb074eeda164
MarcosFonseca <marcosfonceka@gmail.com>
2021-10-05 20:24:26 -0300

    Criação da Controller de Categoria com um método do verbo GET que retornará todas as categorias do banco de dados. Instalação do pacote e configuração do Swagger para documentar as API exportas.

---
+++ src/Mercadinho.Prateleira.API/Controllers/CategoriaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mercadinho.Prateleira.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriaController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            return Ok("funcionou");
+        }
+    }
+}
--- src/Mercadinho.Prateleira.API/Startup.cs
+++ src/Mercadinho.Prateleira.API/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +25,33 @@ namespace Mercadinho.Prateleira.API
 
         public void ConfigureServices(IServiceCollection services)
         {
+            //Ignora referências Ciclicas.
+            services.AddControllers().AddNewtonsoftJson(opt =>
+                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
             services.AddDataRegistration(_configuration);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Mercadinho.Prateleira.API",
+                    Description = "API CRUD para gestão de prateleira do Mercadinho - Projeto do Curso Introdução ao Entity Framework - Digital Inovation One",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Marcos Roberto da Fonseca",
+                        Email = "marcosfonceka@gmail.com",
+                        Url = new Uri("https://github.com/marcosfonceka1711"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,9 +63,19 @@ namespace Mercadinho.Prateleira.API
             }
 
             app.UseRouting();
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Prateleira API");
+                    //c.RoutePrefix = string.Empty;
+                }
+                ) ; 
+                
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
